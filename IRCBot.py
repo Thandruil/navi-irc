5ef872adda9ef2aa57096effa8ea75b8611e9bbd
Erik Gaal <git@erikgaal.nl>
2015-03-02 21:57:32 +0100

    Command parsing

--- IRCBot.py
+++ IRCBot.py
@@ -1,7 +1,10 @@
 import socket
+import re
 
 
 class IRCBot():
+    pattern = re.compile(r'^:(?P<sender>\w+)!(?P<host>\S+)\s(?P<command>\w+)(?:\s(?P<args>.+))?$')
+
     def __init__(self, nickname, server, port=6667):
         self.server = server
         self.running = True
@@ -24,8 +27,8 @@ class IRCBot():
     def leave_channel(self, channel):
         self.send("PART " + channel)
 
-    def message(self, recipient, message):
-        self.send("PRIVMSG " + recipient + " " + message)
+    def send_message(self, channel, message):
+        self.send("PRIVMSG " + channel + " " + message)
 
     def pong(self, message=""):
         self.send("PONG " + message)
@@ -33,7 +36,7 @@ class IRCBot():
     def quit(self):
         self.send("QUIT")
 
-    def on_message(self, message):
+    def on_message(self, sender, channel, message):
         pass
 
     def start(self):
@@ -44,4 +47,16 @@ class IRCBot():
             for line in data.split("\r\n"):
                 if line != "":
                     print(line)
-                    # TODO: implement this
+                    if line.startswith("PING"):
+                        self.pong(line.split()[1][1:])
+                    self.parse_packet(line)
+
+    def parse_packet(self, message):
+        match = self.pattern.match(message)
+        if match is not None:
+            groups = match.groupdict()
+            sender = groups['sender']
+            if groups['command'] == "PRIVMSG":
+                channel, message = groups['args'].split(' ', 1)
+                message = message[1:]
+                self.on_message(sender, channel, message)
